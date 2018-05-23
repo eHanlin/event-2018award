94b87292946938d0bc46461a2bebe4b1623ea057
UJHung <ujhong636@gmail.com>
2018-05-23 14:37:05 +0800

    eHanlin event 2018award

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 var gulp = require('gulp');
+=======
+var gulp = require('gulp');  
+>>>>>>> eHanlin event 2018award
 var rename = require("gulp-rename");
 var fs = require('fs');
 var es = require('event-stream');
@@ -6,6 +10,7 @@ var del = require('del');
 var path = require('path');
 var Q = require('q');
 var util = require('gulp-template-util');
+<<<<<<< HEAD
 var less = require('less');
 
 function buildStyle() {
@@ -89,3 +94,63 @@ gulp.task('package', function() {
     return deferred.promise;
 });
 
+=======
+
+
+var browserify = require('browserify');
+var pug = require('pug');
+var gulpSass = require('gulp-sass');
+
+
+function buildHtml(){
+  return es.map(function(file, cb){
+      file.contents = new Buffer.alloc(pug.renderFile(
+          file.path, { 
+              filename : file.path,
+              pretty : "    "
+          }
+      ));
+      cb(null, file);
+  });
+}
+
+
+
+
+function htmlTask(dest){
+  return function(){
+      return gulp.src('src/pug/**/*.pug')
+          .pipe(buildHtml())
+          .pipe(rename({extname:'.html'}))
+          .pipe(gulp.dest(dest));};    
+}
+
+
+function styleTask(dest){
+  return function(){
+      return gulp.src('src/sass/**/*.sass')
+          .pipe(gulpSass())
+          .pipe(rename({extname:'.css'}))
+          .pipe(gulp.dest(dest));};    
+}
+
+function cleanTask(){
+  return del([
+      'dist',
+      'src/**/*.html',
+      'src/js',
+      'src/css']);
+}
+
+
+
+gulp.task('clean', cleanTask);
+gulp.task('style', styleTask('src/css'));
+gulp.task('html', htmlTask('src'));
+
+
+gulp.task('watch', function() {
+  gulp.watch('src/pug/**/*.pug', ['html']);
+  gulp.watch('src/sass/**/*.sass', ['style']);
+});
+>>>>>>> eHanlin event 2018award
